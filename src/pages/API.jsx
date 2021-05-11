abb152e0e465cb3313cd4afe1acfd678f9b40284
TJ Harrison <tjharrisonjr@gmail.com>
2021-05-10 20:08:16 -0400

    Built out API page

--- src/pages/API.jsx
+++ src/pages/API.jsx
@@ -1,9 +1,28 @@
-import {Segment} from "semantic-ui-react";
+import {Button, Card, CardDescription, CardGroup, CardHeader, CardMeta, Container, Segment} from "semantic-ui-react";
 
 function API() {
   return (
-    <Segment>
-      Feel free to create links using our API instead. Postman collection provided - ready to go out fo the box!
+    <Segment textAlign='center'>
+      <h1>SeeTh.is API</h1>
+      <p>Feel free to create links using our API instead.</p>
+      <CardGroup>
+        <Card>
+          <Button><a href='https://www.getpostman.com/collections/ec45457ccb5280a2d01d'>Get Postman collection</a></Button>
+          <p>All methods are defined and ready to use!</p>
+        </Card>
+        <Card>
+          <CardHeader>Create new short link</CardHeader>
+          <CardMeta>POST https://api.seeth.is/links</CardMeta>
+          <CardDescription>
+            <Segment basic compact textAlign='center'>
+              Expects a JSON body with two fields: link and url. Link can be left blank for an auto-generated short-link.
+            </Segment>
+            <Segment inverted textAlign='left'>
+              <blockquote><pre>{'{'}<br/>&nbsp;&nbsp;"link": "example",<br/>&nbsp;&nbsp;"url": "test.com"<br/>{'}'}</pre></blockquote>
+            </Segment>
+          </CardDescription>
+        </Card>
+      </CardGroup>
     </Segment>
   );
 }
