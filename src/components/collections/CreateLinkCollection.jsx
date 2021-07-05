1a0fbb76c99ed8878e3ca5b3b464be48783e99a7
TJ Harrison <tjharrisonjr@gmail.com>
2021-07-05 17:15:06 -0400

    Visual formatting

--- src/components/collections/CreateLinkCollection.jsx
+++ src/components/collections/CreateLinkCollection.jsx
@@ -1,4 +1,4 @@
-import {Button, Form, Header, Segment} from "semantic-ui-react";
+import {Button, Form, Header, Label, Segment} from "semantic-ui-react";
 import {connect} from "react-redux";
 import {createShortLinkCollection} from "../../store/actions/api";
 import {useFormInput} from "../../hooks/useFormState";
@@ -14,11 +14,10 @@ export const CreatedLinkCollection = ({createShortLinkCollection}) => {
       <Header as="h4">Only portfolio collections are supported currently, more coming soon!</Header>
       <Form onSubmit={submit}>
         <Form.Field>
-          <label>Collection's URL Slug</label>
+          <Label>Collection's URL Slug</Label> Choose wisely, this can't be changed later!
           <Form.Input placeholder='tjharrison' name='name' value={name} onChange={handleNameChange}/>
-          <label>Choose wisely, this can't be changed later! Full URL will https://seeth.is/portfolio/{name.toLowerCase().split(" ").join("-")}</label>
         </Form.Field>
-        <Button type='submit' color="green">Create</Button>
+        <Button type='submit' color="green">Create https://seeth.is/portfolio/{name.toLowerCase().split(" ").join("-")}</Button>
       </Form>
     </Segment>
   );
