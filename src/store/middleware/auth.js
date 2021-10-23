492e29dea3bd97ecbb0fb5e65a42f96480a50524
TJ Harrison <tjharrisonjr@gmail.com>
2021-10-23 11:45:49 -0400

    LINK-44 fix session expiration
    Auto logout after session is expired, actually logout from cognito

--- src/store/middleware/auth.js
+++ src/store/middleware/auth.js
@@ -1,14 +1,18 @@
 import * as actions from '../actions';
 import {sessionService} from 'redux-react-session';
 import * as jwt from 'jsonwebtoken';
+import {logout} from "../actions/auth";
+
+const axios = require('axios').default;
 
 const auth = store => next => async action => {
   switch(action.type){
     case actions.PROCESS_LOGIN:
       const loginData = jwt.decode(action.token);
+      const expiry = loginData.exp * 1000;
       await sessionService.saveSession({
         token: action.token,
-        expiresAt: loginData.exp * 1000
+        expiresAt: expiry
       });
       await sessionService.saveUser({
         userId: action.id,
@@ -16,7 +20,16 @@ const auth = store => next => async action => {
         username: action.username,
         token: action.token
       });
+      //Automatically logout once token is expired
+      setTimeout(() => {
+        store.dispatch(logout());
+      }, expiry);
       action.history.push('/');
+      break;
+    case actions.LOGOUT:
+      sessionService.deleteSession();
+      sessionService.deleteUser();
+      await axios.get(`${process.env.REACT_APP_AUTH_URL}/logout?response_type=token&client_id=${process.env.REACT_APP_AUTH_CLIENT_ID}`, {headers: {'Authorization': store.getState().session.token}})
       break;
     default:
       break;
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import { sessionReducer as session } from 'redux-react-session';
-import auth from './auth';
 import toasts from './toasts';
 import links from './links';
 
 export default combineReducers({
-  auth,
   toasts,
   links,
   session
