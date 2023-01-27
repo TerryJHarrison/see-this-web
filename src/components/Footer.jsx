eb86e58dbc824d64ed88f99c28b38b980988ad4b
TJ Harrison <tjharrisonjr@gmail.com>
2023-01-27 08:51:19 -0500

    Update year, remove funding links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,10 @@ function Footer() {
       <Segment>
       <Grid columns={3}>
         <GridRow>
-          <GridColumn align="left">
-            Keep us funded and free: <a href="https://etherscan.io/address/0x0408d2522d4A121f12bbf6967e8abF31112396EB">ETH</a> - <a href="https://cardanoscan.io/address/addr1q8djvhlpj5hjts6wt6hgaszw8877xrv70x6vdeg0ajgwu2z70jvxu8scju5mwd325y6azde6afndlfnk30yu756mkrsq3kqmr5">ADA</a>
-          </GridColumn>
+          <GridColumn/>
           <GridColumn/>
           <GridColumn align="right">
-            © 2021 TJ Harrison
+            © 2023 TJ Harrison
           </GridColumn>
         </GridRow>
       </Grid>
