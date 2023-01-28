783db8ffe088d9b397afd881d01906f2f3513810
TJ Harrison <tjharrisonjr@gmail.com>
2023-01-28 05:29:38 -0500

    Lazy load all components/pages

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,53 +1,56 @@
+import {lazy, Suspense} from "react";
 import {Container} from "semantic-ui-react";
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Example from "./pages/Example";
-import API from "./pages/API";
-import Collections from "./pages/Collections";
-import Profile from "./pages/Profile";
-import Footer from "./components/Footer";
-import Login from "./components/authentication/Login"
-import Logout from "./components/authentication/Logout"
-import Register from "./components/authentication/Register"
-import LoginCallback from "./components/authentication/LoginCallback"
-import LogoutCallback from "./components/authentication/LogoutCallback"
-import RegisterCallback from "./components/authentication/RegisterCallback"
-import ConfirmCloseAccount from "./components/authentication/ConfirmCloseAccount"
-import PrivateRoute from "./components/authentication/PrivateRoute"
-import ToastDisplay from "./components/ToastDisplay";
 
 import './css/index.css'
 import {connect} from "react-redux";
 
+const Header = lazy(() => import('./components/Header'));
+const Footer = lazy(() => import('./components/Footer'));
+const Home = lazy(() => import('./pages/Home'));
+const Example = lazy(() => import('./pages/Example'));
+const API = lazy(() => import('./pages/API'));
+const Profile = lazy(() => import('./pages/Profile'));
+const Login = lazy(() => import('./components/authentication/Login'));
+const Logout = lazy(() => import('./components/authentication/Logout'));
+const Register = lazy(() => import('./components/authentication/Register'));
+const LoginCallback = lazy(() => import('./components/authentication/LoginCallback'));
+const LogoutCallback = lazy(() => import('./components/authentication/LogoutCallback'));
+const RegisterCallback = lazy(() => import('./components/authentication/RegisterCallback'));
+const ConfirmCloseAccount = lazy(() => import('./components/authentication/ConfirmCloseAccount'));
+const PrivateRoute = lazy(() => import('./components/authentication/PrivateRoute'));
+const ToastDisplay = lazy(() => import('./components/ToastDisplay'));
+
+
 const AppRouter = ({checked}) => {
   return (
     <Router>
-      <Header/>
-      <Container>
-        {checked &&
-        <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/collections" component={Collections}/>
-          <Route path="/api" component={API}/>
-          <Route path="/example" component={Example}/>
-          <Route path="/register" exact component={Register}/>
-          <Route path="/login" exact component={Login}/>
-          <Route path="/logout" exact component={Logout}/>
-          <Route path="/authorize/register" component={RegisterCallback}/>
-          <Route path="/authorize/login" component={LoginCallback}/>
-          <Route path="/authorize/logout" component={LogoutCallback}/>
-          <PrivateRoute path="/profile" exact component={Profile}/>
-          <PrivateRoute path="/profile/close" exact component={ConfirmCloseAccount}/>
-        </Switch>
-        }
-        <ToastDisplay key="toasts-display"/>
-        <Footer/>
-      </Container>
+      <Suspense fallback={null}><Header/></Suspense>
+      <Suspense fallback={null}>
+        <Container>
+          {checked &&
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/api" component={API}/>
+            <Route path="/example" component={Example}/>
+            <Route path="/register" exact component={Register}/>
+            <Route path="/login" exact component={Login}/>
+            <Route path="/logout" exact component={Logout}/>
+            <Route path="/authorize/register" component={RegisterCallback}/>
+            <Route path="/authorize/login" component={LoginCallback}/>
+            <Route path="/authorize/logout" component={LogoutCallback}/>
+            <PrivateRoute path="/profile" exact component={Profile}/>
+            <PrivateRoute path="/profile/close" exact component={ConfirmCloseAccount}/>
+          </Switch>
+          }
+          <ToastDisplay key="toasts-display"/>
+          <Footer/>
+        </Container>
+      </Suspense>
     </Router>
   );
 }
